387fe5779e0dec3fe6b270368340619dfcffdce3
Kiwicai <kiwicai1993hit@gmail.com>
2016-05-03 12:54:24 -0400

    codes + files
    business

--- PocketBox/app/src/main/java/com/example/kiwicai/pocketbox/exception/FixException.java
+++
@@ -1,7 +0,0 @@
-package com.example.kiwicai.pocketbox.exception;
-/**
- * Created by hongbao on 2016/3/22.
- */
-public interface FixException {
-	void fix(int errno);
-}
--- PocketBox/app/src/main/java/com/example/kiwicai/pocketbox/proxy/MailDetailConstant.java
+++
@@ -1,9 +0,0 @@
-package com.example.kiwicai.pocketbox.proxy;
-
-/**
- * Created by tao-macbook on 4/6/16.
- */
-public interface MailDetailConstant {
-    String EMAIL = "message";
-    String MAILBOX = "box";
-}
